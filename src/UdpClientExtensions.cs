67671c8ed3185bc7a2706958b82c173a0e759cbd
iheartcsharp <38707825+iheartcsharp@users.noreply.github.com>
2025-05-18 21:46:59 -0400

    Support latest net9 and latest c# language

--- src/UdpClientExtensions.cs
+++ src/UdpClientExtensions.cs
@@ -13,10 +13,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -36,7 +36,7 @@ using System.Threading.Tasks;
 
 namespace MonoTorrent
 {
-#if NET472 || NET5_0 || NET6_0 || NETCOREAPP3_0 || NETSTANDARD2_0 || NETSTANDARD2_1
+#if NET472 || NET5_0 || NET6_0 || NET7_0 || NET8_0 || NET9_0 || NETCOREAPP3_0 || NETSTANDARD2_0 || NETSTANDARD2_1
     static class UdpClientExtensions
     {
         public static Task<int> SendAsync (this UdpClient client, ReadOnlyMemory<byte> datagram, int bytes, IPEndPoint? endPoint)
